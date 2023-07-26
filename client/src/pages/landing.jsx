10b337594e25eedd61025144644fc005e0f311d4
Nico <nico.tramontina@gmail.com>
2023-07-26 04:21:23 -0300

    feat(frontend-landing): img

--- client/src/pages/landing.jsx
+++ client/src/pages/landing.jsx
@@ -1,23 +1,22 @@
 import { Link } from "react-router-dom";
 import styles from "./landing.module.css";
+import FoodLanding from "../images/FoodLanding.png";
 
 function Landing() {
   return (
     <div className={styles.hero}>
       <div className={styles.text}>
         <h1>Bienvenido a Mi Food app</h1>
-        <h2>
-          {" "}
-          Aqui podras encontrar, filtrar y ordenar tus recetas segun tus gustos!
-        </h2>
+        <h3> Aqui podras encontrar, filtrar y ordenar</h3>
+        <h3>tus recetas segun tus gustos!</h3>
         <div>
           <Link to="/home">
             <button className={styles.button}>Go!</button>
           </Link>
         </div>
       </div>
-      <div className={styles.img}>
-        <img src="" alt="" />
+      <div className={styles.imgcontainer}>
+        <img className={styles.img} src={FoodLanding} alt="" />
       </div>
     </div>
   );
