cf625b2717c8d321dda45e77cb4e2138f787629e
Nico <nico.tramontina@gmail.com>
2023-08-25 05:23:34 -0300

    refactor(backend): models error

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -3,32 +3,36 @@ const { DataTypes } = require("sequelize");
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define("recipe", {
-    id: {
-      type: DataTypes.UUID,
-      primaryKey: true,
-      defaultValue: DataTypes.UUIDV4,
-      allowNull: false,
+  sequelize.define(
+    "recipe",
+    {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      summary: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      healthScore: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      stepByStep: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
     },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    image: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    summary: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    healthScore: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    stepByStep: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  });
+    { timestamps: false }
+  );
 };
